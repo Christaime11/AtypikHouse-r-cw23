76479d4387a139bc9fa369e9e91b334a576c81aa
Christaime <linusague@gmail.com>
2021-03-24 04:39:27 +0100

    IsoMetric bg on login page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent } from './views/layout/base/base.component';
 import { AuthGuard } from './core/guard/auth.guard';
 import { ErrorPageComponent } from './views/pages/error-page/error-page.component';
-import { HomeComponent } from './views/pages/public/home/home.component';
-import { HabitatsComponent } from './views/pages/public/habitats/habitats.component';
 
 
 const routes: Routes = [
@@ -38,31 +36,6 @@ const routes: Routes = [
       {
         path: 'apps',
         loadChildren: () => import('./views/pages/apps/apps.module').then(m => m.AppsModule)
-      },
-      {
-        path: 'ui-components',
-        loadChildren: () => import('./views/pages/ui-components/ui-components.module').then(m => m.UiComponentsModule)
-      },
-      {
-        path: 'advanced-ui',
-        loadChildren: () => import('./views/pages/advanced-ui/advanced-ui.module').then(m => m.AdvancedUiModule)
-      },
-      {
-        path: 'form-elements',
-        loadChildren: () => import('./views/pages/form-elements/form-elements.module').then(m => m.FormElementsModule)
-      },
-      {
-        path: 'advanced-form-elements',
-        // tslint:disable-next-line:max-line-length
-        loadChildren: () => import('./views/pages/advanced-form-elements/advanced-form-elements.module').then(m => m.AdvancedFormElementsModule)
-      },
-      {
-        path: 'tables',
-        loadChildren: () => import('./views/pages/tables/tables.module').then(m => m.TablesModule)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/pages/icons/icons.module').then(m => m.IconsModule)
       },
       {
         path: 'general',
--- client/src/app/views/pages/general/general.module.ts
+++ client/src/app/views/pages/general/general.module.ts
@@ -6,12 +6,7 @@ import { FeahterIconModule } from '../../../core/feather-icon/feather-icon.modul
 import { NgbAccordionModule, NgbDropdownModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { GeneralComponent } from './general.component';
-import { BlankComponent } from './blank/blank.component';
-import { FaqComponent } from './faq/faq.component';
-import { InvoiceComponent } from './invoice/invoice.component';
 import { ProfileComponent } from './profile/profile.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { TimelineComponent } from './timeline/timeline.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
@@ -25,35 +20,15 @@ const routes: Routes = [
         pathMatch: 'full'
       },
       {
-        path: 'blank-page',
-        component: BlankComponent
-      },
-      {
-        path: 'faq',
-        component: FaqComponent
-      },
-      {
-        path: 'invoice',
-        component: InvoiceComponent
-      },
-      {
         path: 'profile',
         component: ProfileComponent
-      },
-      {
-        path: 'pricing',
-        component: PricingComponent
-      },
-      {
-        path: 'timeline',
-        component: TimelineComponent
       }
     ]
   }
 ]
 
 @NgModule({
-  declarations: [GeneralComponent, BlankComponent, FaqComponent, InvoiceComponent, ProfileComponent, PricingComponent, TimelineComponent],
+  declarations: [GeneralComponent, ProfileComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
