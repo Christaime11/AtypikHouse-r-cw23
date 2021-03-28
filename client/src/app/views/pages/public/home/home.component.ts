091f203b97875860ad43a33485e7eb827fb72449
Christaime <linusague@gmail.com>
2021-03-28 01:59:17 +0100

    Date search

--- client/src/app/views/pages/public/home/home.component.ts
+++ client/src/app/views/pages/public/home/home.component.ts
@@ -1,14 +1,98 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbDate, NgbCalendar, NgbDateStruct, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {
+  NgbDate,
+  NgbCalendar,
+  NgbDateStruct,
+  NgbDatepickerConfig,
+  NgbDateParserFormatter,
+  NgbDateAdapter
+} from '@ng-bootstrap/ng-bootstrap';
+
+/**
+ * This Service handles how the date is represented in scripts i.e. ngModel.
+ */
+@Injectable()
+export class CustomAdapter extends NgbDateAdapter<string> {
+
+  readonly DELIMITER = '-';
+
+  fromModel(value: string | null): NgbDateStruct | null {
+    if (value) {
+      let date = value.split(this.DELIMITER);
+      return {
+        year : parseInt(date[0], 10),
+        month : parseInt(date[1], 10),
+        day : parseInt(date[2], 10),
+      };
+    }
+    return null;
+  }
+
+  toModel(date: NgbDateStruct | null): string | null {
+    return date ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day : null;
+  }
+}
+
+/**
+ * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
+ */
+@Injectable()
+export class CustomDateParserFormatter extends NgbDateParserFormatter {
+
+  readonly DELIMITER = '-';
+
+  parse(value: string): NgbDateStruct | null {
+    if (value) {
+      let date = value.split(this.DELIMITER);
+      return {
+        year : parseInt(date[0], 10),
+        month : parseInt(date[1], 10),
+        day : parseInt(date[2], 10),
+      };
+    }
+    return null;
+  }
+
+  format(date: NgbDateStruct | null): string {
+    return date ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day : '';
+  }
+}
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+
+  // NOTE: For this example we are only providing current component, but probably
+  // NOTE: you will want to provide your main App Module
+  providers: [
+    {provide: NgbDateAdapter, useClass: CustomAdapter},
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter }
+  ]
 })
+
+
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  selectedDate: NgbDateStruct;
+  selectedDate2: NgbDateStruct;
+  minDate = undefined;
+
+  constructor(private config: NgbDatepickerConfig) {
+    const current = new Date();
+    config.minDate = {
+      year: current.getFullYear(),
+      month: current.getMonth() + 1,
+      day: current.getDate()
+    };
+    config.maxDate = {
+      year: current.getFullYear() + 1,
+      month: current.getMonth() + 1,
+      day: current.getDate()
+    };
+    config.outsideDays = 'hidden';
     }
 
   ngOnInit(): void {
--- client/src/app/views/pages/public/public.module.ts
+++ client/src/app/views/pages/public/public.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { NgbDatepickerModule, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'; // 1
+
 
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
 import { HabitatsComponent } from './habitats/habitats.component';
+import { FeahterIconModule } from '../../../core/feather-icon/feather-icon.module';
 
 
 const routes: Routes = [
@@ -30,7 +34,10 @@ const routes: Routes = [
   declarations: [PublicComponent, HomeComponent, HabitatsComponent],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    NgbDatepickerModule,
+    FormsModule,
+    FeahterIconModule
   ]
 })
 export class PublicModule { }
